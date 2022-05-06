a4d641526cbbc8a840e1e6b0a971280d514b0caf
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-06 16:29:05 -0700

    WIP. add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,15 @@ function PopperRoot() {
 function App() {
     return (<>
         <PopperRoot />
-        <div className="min-h-full overflow-hidden bg-slate-50">
-            <div className="">Fresh start</div>
+        <div className="min-h-full overflow-hidden text-slate-100 bg-slate-900">
+            <div className="h-screen flex flex-col">
+                <header className="p-4 text-3xl uppercase">Fresh start</header>
+                <main className="mx-4 flex-1 flex flex-col justify-evenly">
+                    <section className="p-4 border border-slate-500">1</section>
+                    <section className="p-4 border border-slate-500">2</section>
+                    <section className="p-4 border border-slate-500">3</section>
+                </main>
+            </div>
         </div>
     </>);
 }
