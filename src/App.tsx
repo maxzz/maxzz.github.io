23d38459a515fa0446809cde3d7c7dff3f2e74ca
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-06 17:40:40 -0700

    WIP. switch to declarative section definitions

--- src/App.tsx
+++ src/App.tsx
@@ -12,18 +12,27 @@ function PopperRoot() {
 import previewDropzon from "./assets/pages/dropzone-2022-03-04_15-29-37.png";
 
 type ProjectTitleProps = {
-    url: string;
-    preview: string;
     caption: string;
     info: string;
+    url: string;
+    preview: string;
 };
+
+const projectsList: ProjectTitleProps[] = [
+    {
+        caption: "PMIT",
+        info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae nisi enim veniam illum totam laborum maxime assumenda facere aspernatur voluptatum a nihil dicta officia, suscipit dignissimos voluptatibus repudiandae inventore veritatis?",
+        url: "https://maxzz.github.io/dropzone",
+        preview: previewDropzon,
+    },
+];
 
 function ProjectTitle({ url, preview, caption, info }: ProjectTitleProps) {
     return (
         <section className="p-4 bg-slate-800">
             <a className="flex items-center justify-between space-x-4" href={url} target="_blank">
                 <div className="self-start">
-                    <div className="hoverurl">{caption}</div>
+                    <div className="hoverurl inline-block">{caption}</div>
                     <div className="">{info}</div>
                 </div>
                 <div className="flex-none w-24 h-auto"><img src={preview} alt="" /></div>
@@ -39,12 +48,7 @@ function App() {
             <div className="h-screen flex flex-col">
                 <header className="p-4 text-3xl uppercase">Fresh start</header>
                 <main className="mx-4 flex-1 flex flex-col justify-evenly">
-                    <ProjectTitle
-                        url="https://maxzz.github.io/dropzone"
-                        preview={previewDropzon}
-                        caption="PMIT"
-                        info="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae nisi enim veniam illum totam laborum maxime assumenda facere aspernatur voluptatum a nihil dicta officia, suscipit dignissimos voluptatibus repudiandae inventore veritatis?"
-                    />
+                    <ProjectTitle {...projectsList[0]} />
 
                     <section className="p-4 bg-slate-800">2</section>
                     <section className="p-4 bg-slate-800">3</section>
