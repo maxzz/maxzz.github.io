b7ed65437f700a0df3c80dd422a761a2cad3c518
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-06 17:20:33 -0700

    WIP. add a section prototype

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#### maxzz.github.io
+## maxzz.github.io
 
-* develop on master branch
-* from master branch copy dist folder to the project root
+My cross refs website.
+
--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,19 @@ function App() {
             <div className="h-screen flex flex-col">
                 <header className="p-4 text-3xl uppercase">Fresh start</header>
                 <main className="mx-4 flex-1 flex flex-col justify-evenly">
-                    <section className="p-4 border border-slate-500">1</section>
-                    <section className="p-4 border border-slate-500">2</section>
-                    <section className="p-4 border border-slate-500">3</section>
+
+                    <section className="p-4 bg-slate-800">
+                        <a className="flex items-center justify-between space-x-4" href="https://maxzz.github.io/dropzone" target="_blank">
+                            <div className="self-start">
+                                <div className="hoverurl">PMIT</div>
+                                <div className="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae nisi enim veniam illum totam laborum maxime assumenda facere aspernatur voluptatum a nihil dicta officia, suscipit dignissimos voluptatibus repudiandae inventore veritatis?</div>
+                            </div>
+                            <div className="flex-none w-24 h-auto"><img src="src/assets/pages/dropzone-2022-03-04_15-29-37.png" alt="" /></div>
+                        </a>
+                    </section>
+                    
+                    <section className="p-4 bg-slate-800">2</section>
+                    <section className="p-4 bg-slate-800">3</section>
                 </main>
             </div>
         </div>
