ce73e7814c7b5ebec376dd41fa1569598a755f98
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-12 17:10:29 -0700

    WIP. Section2_Main. switch card from flex to grid

--- src/components/Section2_Main.tsx
+++ src/components/Section2_Main.tsx
@@ -4,8 +4,8 @@ import { sections } from '@/store/store-initials';
 
 function ProjectTile({ caption, description, urlGithub, urlDemo, previewUrl, }: ProjectType) {
     return (
-        <section className="p-4 bg-slate-800 rounded">
-            <div className="flex justify-between">
+        <section className="p-4 grid grid-rows-[auto_1fr_auto] grid-cols-[1fr_auto] gap-x-4 gap-y-2 bg-slate-800 rounded">
+            {/* <div className="flex justify-between">
 
                 <div className="flex flex-col items-start justify-between">
                     <div className="">
@@ -26,8 +26,29 @@ function ProjectTile({ caption, description, urlGithub, urlDemo, previewUrl, }: 
                 <div className="flex-none w-24 h-auto">
                     <img src={previewUrl} alt="" />
                 </div>
-            </div>
-        </section>
+            </div> */}
+
+
+                <div className="">
+                    <div className="hoverurl inline-block uppercase">
+                        {caption}
+                    </div>
+                </div>
+
+                <div className="text-sm">
+                    {description}
+                </div>
+
+                <div className="text-sm flex space-x-2">
+                    <a className="" href={urlGithub} target="_blank">Github</a>
+                    <a className="" href={urlDemo} target="_blank">Demo</a>
+                </div>
+
+                <div className="col-start-2 col-end-3 row-span-full place-self-center w-24 h-auto">
+                    <img src={previewUrl} alt="" />
+                </div>
+
+        </section >
     );
 }
 
