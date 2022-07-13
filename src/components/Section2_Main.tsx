da8ae19a658893b2c708f5aa264c5c8ac75dc63d
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-12 17:22:21 -0700

    WIP. Section2_Main. update card grid

--- src/components/Section2_Main.tsx
+++ src/components/Section2_Main.tsx
@@ -4,51 +4,24 @@ import { sections } from '@/store/store-initials';
 
 function ProjectTile({ caption, description, urlGithub, urlDemo, previewUrl, }: ProjectType) {
     return (
-        <section className="p-4 grid grid-rows-[auto_1fr_auto] grid-cols-[1fr_auto] gap-x-4 gap-y-2 bg-slate-800 rounded">
-            {/* <div className="flex justify-between">
+        <section className="flex flex-col">
+            <div className="pb-1 uppercase">
+                {caption}
+            </div>
 
-                <div className="flex flex-col items-start justify-between">
-                    <div className="">
-                        <div className="hoverurl inline-block uppercase">
-                            {caption}
-                        </div>
-                        <div className="mt-2 text-sm">
-                            {description}
-                        </div>
-                    </div>
-
-                    <div className="text-sm flex space-x-2">
-                        <a className="" href={urlGithub} target="_blank">Github</a>
-                        <a className="" href={urlDemo} target="_blank">Demo</a>
-                    </div>
-                </div>
-
-                <div className="flex-none w-24 h-auto">
-                    <img src={previewUrl} alt="" />
-                </div>
-            </div> */}
-
-
-                <div className="">
-                    <div className="hoverurl inline-block uppercase">
-                        {caption}
-                    </div>
-                </div>
-
+            <div className="flex-1 p-4 grid grid-rows-[1fr_auto] grid-cols-[1fr_auto] gap-x-4 gap-y-2 bg-slate-800 rounded">
                 <div className="text-sm">
                     {description}
                 </div>
-
                 <div className="text-sm flex space-x-2">
-                    <a className="" href={urlGithub} target="_blank">Github</a>
-                    <a className="" href={urlDemo} target="_blank">Demo</a>
+                    <div className="h-5"><a className="hoverurl" href={urlGithub} target="_blank">Github</a></div>
+                    <div className="h-5"><a className="hoverurl" href={urlDemo} target="_blank">Demo</a></div>
                 </div>
-
                 <div className="col-start-2 col-end-3 row-span-full place-self-center w-24 h-auto">
                     <img src={previewUrl} alt="" />
                 </div>
-
-        </section >
+            </div >
+        </section>
     );
 }
 
@@ -59,7 +32,7 @@ function Section({ section }: { section: SectionType; }) {
                 {section.name}
             </div>
 
-            <div className="mx-4 grid grid-cols-[repeat(auto-fill,minmax(320px,1fr))] gap-2 overflow-y-auto">
+            <div className="mx-4 grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-2 overflow-y-auto">
                 {section.projects.map((project, idx) => (
                     <ProjectTile {...project} key={idx} />
                 ))}
