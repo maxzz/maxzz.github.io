ba530f0a69ee0bf757718abe64ccde34137b337a
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-12 17:29:44 -0700

    WIP. Section2_Main. update sections grid

--- src/components/Section2_Main.tsx
+++ src/components/Section2_Main.tsx
@@ -5,11 +5,11 @@ import { sections } from '@/store/store-initials';
 function ProjectTile({ caption, description, urlGithub, urlDemo, previewUrl, }: ProjectType) {
     return (
         <section className="flex flex-col">
-            <div className="pb-1 uppercase">
+            <div className="pb-1 text-slate-300 uppercase">
                 {caption}
             </div>
 
-            <div className="flex-1 p-4 grid grid-rows-[1fr_auto] grid-cols-[1fr_auto] gap-x-4 gap-y-2 bg-slate-800 rounded">
+            <div className="flex-1 p-4 grid grid-rows-[1fr_auto] grid-cols-[1fr_auto] gap-x-4 gap-y-2 text-slate-400 bg-slate-800 rounded">
                 <div className="text-sm">
                     {description}
                 </div>
@@ -27,12 +27,12 @@ function ProjectTile({ caption, description, urlGithub, urlDemo, previewUrl, }: 
 
 function Section({ section }: { section: SectionType; }) {
     return (
-        <div className="w-full max-w-7xl">
-            <div className="">
-                {section.name}
+        <div className="w-full max-w-7xl mx-4 grid grid-cols-[auto_minmax(0,1fr)] gap-4">
+            <div className="text-2xl p-4 border-slate-400 border rounded">
+                {section.name} v
             </div>
 
-            <div className="mx-4 grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-2 overflow-y-auto">
+            <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-2 overflow-y-auto">
                 {section.projects.map((project, idx) => (
                     <ProjectTile {...project} key={idx} />
                 ))}
@@ -43,7 +43,7 @@ function Section({ section }: { section: SectionType; }) {
 
 export function Section2_Main() {
     return (
-        <main className="flex-1 flex flex-col items-center space-y-12 overflow-auto">
+        <main className="flex-1 py-4 flex flex-col items-center space-y-12 overflow-auto">
             {sections.map((section, idx) => <Section section={section} key={idx} />)}
         </main>
     );
