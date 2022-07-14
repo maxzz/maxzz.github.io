c61c12388cff08e83fa375d4e7f23b6464437f71
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-14 14:34:23 -0700

    WIP. store. add getValues and setValues to AtomsFamily2

--- src/store/store.ts
+++ src/store/store.ts
@@ -9,7 +9,7 @@ namespace Storage {
     type Store = {
         open: {
             source: boolean;
-        }
+        };
     };
 
     export let initialData: Store = {
@@ -62,9 +62,10 @@ export const sectionOpenAtoms = AtomsFamily<boolean>(false);
 
 //////
 
-const AtomsFamily2 = <T extends boolean>(initialValue: T, initAtom: (param: T) => PrimitiveAtom<T> ) => {
-    const map = new Map<string, PrimitiveAtom<T>>();
-    return (key: string) => {
+const AtomsFamily2 = <T>(initialValue: T, initAtom: (param: T) => PrimitiveAtom<T>) => {
+    let map = new Map<string, PrimitiveAtom<T>>();
+    
+    const getAtom = (key: string) => {
         let value = map.get(key);
         if (!value) {
             value = initAtom(initialValue);
@@ -72,6 +73,22 @@ const AtomsFamily2 = <T extends boolean>(initialValue: T, initAtom: (param: T) =
         }
         return value;
     };
+
+    getAtom.setValues = (newMap: Record<string, T>) => {
+        const entries = Object.entries(newMap || {});
+        map = new Map((entries.map(([key, value]) => [key, initAtom(value)])));
+    }
+
+    getAtom.getValues = (get: Getter): Record<string, T> => {
+        const entries = map.entries();
+        const rv: Record<string, T> = {};
+        for (const [key, value] of entries) {
+            rv[key] = get(value);
+        }
+        return rv;
+    }
+
+    return getAtom;
 };
 
 export const sectionOpenAtoms2 = AtomsFamily2<boolean>(false, (param: boolean) => atom(param));
