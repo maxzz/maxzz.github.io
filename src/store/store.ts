3885332059f0598932762f150d841203a56f1405
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-13 23:50:40 -0700

    WIP. store. add localStorage skeleton

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,51 @@
-import { atom, PrimitiveAtom } from "jotai";
-import { atomFamily } from 'jotai/utils';
+import { atom, Getter, PrimitiveAtom } from "jotai";
+import debounce from "@/utils/debounce";
+
+//#region LocalStorage
+
+namespace Storage {
+    const KEY = 'react-maxzz-io-01';
+
+    type Store = {
+        open: {
+            source: boolean;
+        }
+    };
+
+    export let initialData: Store = {
+        open: {
+            source: false,
+        },
+    };
+
+    function load() {
+        const s = localStorage.getItem(KEY);
+        if (s) {
+            try {
+                let obj = JSON.parse(s) as Store;
+                initialData = { ...initialData, ...obj };
+            } catch (error) {
+            }
+        }
+
+        //initialData.vaultData.shapes = initialData.vaultData?.shapes?.length ? initialData.vaultData.shapes : defaultShapes;
+    }
+    load();
+
+    export const saveDebounced = debounce(function _save(get: Getter) {
+        let newStore: Store = {
+            open: {
+                source: get(sourceAtom),
+            },
+        };
+        localStorage.setItem(KEY, JSON.stringify(newStore));
+    }, 1000);
+
+    export const save = ({ get }: { get: Getter; }) => Storage.saveDebounced(get);
+}
+
+//#endregion LocalStorage
+
 
 const AtomFactory = (initialValue: boolean = false) => {
     const map: Record<string, PrimitiveAtom<boolean>> = {};
@@ -10,5 +56,7 @@ const AtomFactory = (initialValue: boolean = false) => {
         return map[name];
     };
 };
+
+export const sourceAtom = atom<boolean>(false);
 
 export const sectionOpenAtoms = AtomFactory();
