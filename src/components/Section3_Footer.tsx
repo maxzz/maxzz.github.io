58984dba2377519eb2ac1e0496d58a23b3ca863d
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-14 19:23:37 -0700

    WIP. Section3_Footer. update layout

--- src/components/Section3_Footer.tsx
+++ src/components/Section3_Footer.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
+import { classNames } from '@/utils/classnames';
+import { IconSunnyvale } from './UI/UIIcons';
 
-export function Section3_Footer() {
+export function Section3_Footer({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     return (
-        <div className="h-12 bg-black"></div>
+        <div className={classNames("h-12 text-xs bg-black text-slate-600 flex items-center justify-center", className)} {...rest}>
+            <p>Created by Max Zakharzhevskiy.</p>
+            <IconSunnyvale className="w-5 h-5" />
+            <p>Open sourced on <a className="underline" href="https://github.com/maxzz/maxzz.github.io" target="_blank" rel="noopener">Github</a>.</p>
+        </div>
     );
 }
