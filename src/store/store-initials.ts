540f3ad50ed223053fffc6c2bf557d43679f0106
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-16 17:57:51 -0700

    WIP. image bundler. add use of reduced image

--- src/store/store-initials.ts
+++ src/store/store-initials.ts
@@ -1,13 +1,13 @@
 import { ProjectType, SectionType } from "./store-types";
 
-import previewDropzon from "../assets/pages/dropzone-2022-03-04_15-29-37.png";
-import simpleBorderRadius from "../assets/pages/simple-border-radius22-2022-07-08_18-46-46.png";
-import red3Curves from "../assets/pages/red3-curves-2021-08-18_3-57-11.png";
-import borderRadiusGenerator from "../assets/pages/border-radius-generator-2020-12-18_4-44-41.png";
-import red3Path from "../assets/pages/red3-path-2021-09-04_19-45-16.png";
-import colorNames22 from "../assets/pages/color-names22-2022-06-02_3-49-34.png";
-import svgPath from "../assets/pages/svg-path-2022-02-03_14-18-21-sub-paths.png";
-import testPmDomainLogins22 from "../assets/pages/test-pm-domain-logins22-2022-06-29_1-32-01.png";
+//import previewDropzon from "@/assets/pages/dropzone-2022-03-04_15-29-37.png";
+import simpleBorderRadius from "@/assets/pages/simple-border-radius22-2022-07-08_18-46-46.png";
+import red3Curves from "@/assets/pages/red3-curves-2021-08-18_3-57-11.png";
+import borderRadiusGenerator from "@/assets/pages/border-radius-generator-2020-12-18_4-44-41.png";
+import red3Path from "@/assets/pages/red3-path-2021-09-04_19-45-16.png";
+import colorNames22 from "@/assets/pages/color-names22-2022-06-02_3-49-34.png";
+import svgPath from "@/assets/pages/svg-path-2022-02-03_14-18-21-sub-paths.png";
+import testPmDomainLogins22 from "@/assets/pages/test-pm-domain-logins22-2022-06-29_1-32-01.png";
 
 import srcset from '@/assets/pages/test-pm-domain-logins22-2022-06-29_1-32-01.png?preset=thumbnail';
 
@@ -20,7 +20,9 @@ export const projectsDp: ProjectType[] = [
         description: "Password manager investigation tool as an experiment with data-driven user interfaces (D2UI).",
         urlGithub: "https://github.com/maxzz/dropzone",
         urlDemo: "https://maxzz.github.io/dropzone",
-        previewUrl: previewDropzon,
+        // previewUrl: previewDropzon,
+        // previewUrl: "@/assets/pages/dropzone-2022-03-04_15-29-37.png?preset=thumbnail&src",
+        previewUrl: srcset[srcset.length - 1].src || '',
     },
     {
         caption: "test-pm-domain-logins22",
