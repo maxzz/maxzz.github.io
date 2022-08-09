0c980504f5de5e63799ad666d91b4e065fd2c6b8
Max Zakharzhevskiy <maxzz@msn.com>
2022-08-08 19:02:17 -0700

    WIP. Section2_Main. add copy feedback icon

--- src/components/UI/UIIcons.tsx
+++ src/components/UI/UIIcons.tsx
@@ -279,6 +279,15 @@ export function IconGrid({ title, ...rest }: HTMLAttributes<SVGSVGElement> & SVG
     );
 }
 
+export function IconCheckFrameless({ title, ...rest }: HTMLAttributes<SVGSVGElement> & SVGProps<SVGSVGElement>) {
+    return (
+        <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" {...rest}>
+            {title && <title>{title}</title>}
+            <path d="m3.1 12 5.94 5.94L20.9 6.06" />
+        </svg>
+    );
+}
+
 /*
 export function UISymbolsDefs() {
     return (
