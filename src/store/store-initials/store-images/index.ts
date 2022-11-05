0e5dd81f2b4f92c48137928d3aef01bbc639bd77
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-04 18:55:42 -0700

    WIP. store-initials. add images-vue.ts

--- src/store/store-initials/store-images/index.ts
+++ src/store/store-initials/store-images/index.ts
@@ -32,21 +32,6 @@ import reactAdminAddressBook from "@/assets/pages/react/react-admin-address-book
 import maxzz from "@/assets/pages/react/maxzz-main.svg?preset=thumbnail";
 import maxzzGithubIo from "@/assets/previews/2022-07-12_18-24-45.png?preset=thumbnail";
 
-//ux, vue
-import colorNames from "@/assets/pages/vue/color-names-2020-12-02_4-55-29.png?preset=thumbnail";
-import ngonGen from "@/assets/pages/vue/ngon-gen-preview-20200512.png?preset=thumbnail";
-import animatedSidebar from "@/assets/pages/vue/animated-sidebar-2022-07-18_1-00-43.png?preset=thumbnail";
-import chSpy from "@/assets/pages/vue/ch-spy.png?preset=thumbnail";
-import coursehuntersCourseListBrowserCtx from "@/assets/pages/vue/coursehunters-course-list-browser-ctx-2021-05-07_19-17-13.png?preset=thumbnail";
-import autoDatGui from "@/assets/pages/vue/auto-dat-gui-2020-11-15_21-24-52.png?preset=thumbnail";
-import backTo90sHtml from "@/assets/pages/vue/back-to-90s-html-2020-12-12_19-48-25.png?preset=thumbnail";
-import vue26SiteExamplesTodos from "@/assets/pages/vue/vue26-site-examples-todos-preview.png?preset=thumbnail";
-import svgGradients from "@/assets/pages/vue/svg-gradients-2020-10-20_21-25-34.png?preset=thumbnail";
-import revuePlayground from "@/assets/pages/vue/revue-playground-2021-03-11_19-21-14.png?preset=thumbnail";
-import svgIconsVite from "@/assets/pages/vue/svg-icons-vite-2022-07-18_17-58-31.png?preset=thumbnail";
-import simpleSiriwave from "@/assets/pages/vue/simple-siriwave-2020-12-15_17-59-36.png?preset=thumbnail";
-import svgBkgStripes from "@/assets/pages/vue/svg-bkg-stripes-2022-07-18_18-25-35.png?preset=thumbnail";
-
 export {
     reactNoPreview,
 };
@@ -89,19 +74,8 @@ export {
 //npm
 export * from './images-npm';
 
+//dp
+export * from './images-dp';
+
 //ux, vue
-export {
-    colorNames,
-    ngonGen,
-    animatedSidebar,
-    chSpy,
-    coursehuntersCourseListBrowserCtx,
-    autoDatGui,
-    backTo90sHtml,
-    vue26SiteExamplesTodos,
-    svgGradients,
-    revuePlayground,
-    svgIconsVite,
-    simpleSiriwave,
-    svgBkgStripes,
-};
+export * from './images-vue';
