fdf6ef1b2a7c110df7133166be9ccb1b3d1da63f
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-04 18:54:14 -0700

    WIP. store-initials. add images-dp.ts

--- src/store/store-initials/store-images/index.ts
+++ src/store/store-initials/store-images/index.ts
@@ -32,16 +32,6 @@ import reactAdminAddressBook from "@/assets/pages/react/react-admin-address-book
 import maxzz from "@/assets/pages/react/maxzz-main.svg?preset=thumbnail";
 import maxzzGithubIo from "@/assets/previews/2022-07-12_18-24-45.png?preset=thumbnail";
 
-//dp
-import previewDropzon from "@/assets/pages/dp/dropzone-2022-03-04_15-29-37.png?preset=thumbnail";
-import testPm from "@/assets/pages/dp/test-pm.png?preset=thumbnail";
-import testPmSecond from "@/assets/pages/dp/test-pm-second-2022-05-21_20-03-21.png?preset=thumbnail";
-import testPmDomainLogins22 from "@/assets/pages/dp/test-pm-domain-logins22-2022-06-29_1-32-01.png?preset=thumbnail";
-import pageQa2 from "@/assets/pages/dp/page-qa2-2022-04-16_16-49-18.png?preset=thumbnail";
-import testPmDomainLogins from "@/assets/pages/dp/test-pm-domain-logins-2022-03-01_19-09-50.jpg?preset=thumbnail";
-import pageExtensionInstallation from "@/assets/pages/dp/page-extension-installation-2022-06-24_17-21-17.png?preset=thumbnail";
-
-
 //ux, vue
 import colorNames from "@/assets/pages/vue/color-names-2020-12-02_4-55-29.png?preset=thumbnail";
 import ngonGen from "@/assets/pages/vue/ngon-gen-preview-20200512.png?preset=thumbnail";
@@ -95,16 +85,6 @@ export {
     maxzzGithubIo,
 };
 
-//dp
-export {
-    previewDropzon,
-    testPm,
-    testPmSecond,
-    testPmDomainLogins22,
-    pageQa2,
-    testPmDomainLogins,
-    pageExtensionInstallation,
-};
 
 //npm
 export * from './images-npm';
