1828bdcbad48964c00fdeae72453441993cf10ab
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-04 17:30:56 -0700

    WIP. store-initials. update dp preview locations

--- src/store/store-images.ts
+++ src/store/store-images.ts
@@ -4,23 +4,18 @@ import red3Curves from "@/assets/pages/red3-curves-2021-08-18_3-57-11.png?preset
 import borderRadiusGenerator from "@/assets/pages/border-radius-generator-2020-12-18_4-44-41.png?preset=thumbnail";
 import red3Path from "@/assets/pages/red3-path-2021-09-04_19-45-16.png?preset=thumbnail";
 import colorNames22 from "@/assets/pages/color-names22-2022-06-02_3-49-34.png?preset=thumbnail";
-import testPmDomainLogins22 from "@/assets/pages/test-pm-domain-logins22-2022-06-29_1-32-01.png?preset=thumbnail";
 import svgPath from "@/assets/pages/svg-path-2022-02-03_14-18-21-sub-paths.png?preset=thumbnail";
-import pageExtensionInstallation from "@/assets/pages/page-extension-installation-2022-06-24_17-21-17.png?preset=thumbnail";
-import testPmSecond from "@/assets/pages/test-pm-second-2022-05-21_20-03-21.png?preset=thumbnail";
 import levaUiDynamicBoxes from "@/assets/pages/leva-ui-dynamic-boxes-2022-05-03_18-08-19.png?preset=thumbnail";
 import editorsSvgShapes22Template from "@/assets/pages/editors-svg-shapes22-template-2022-04-25_14-17-36.png?preset=thumbnail";
 import ngonGen22 from "@/assets/pages/ngon-gen22-2022-06-11_18-09-01.png?preset=thumbnail";
 import volumeIllusion from "@/assets/pages/volume-illusion-2021-05-04_18-32-05.png?preset=thumbnail";
-import pageQa2 from "@/assets/pages/page-qa2-2022-04-16_16-49-18.png?preset=thumbnail";
+
 import cursors from "@/assets/pages/cursors-2022-02-20_15-30-06.png?preset=thumbnail";
 import noiseGen from "@/assets/pages/noise-gen-2021-06-14_17-38-45.png?preset=thumbnail";
 import npm from "@/assets/pages/npm-node-package-manager.webp?preset=thumbnail";
 import animatedSidebar from "@/assets/pages/animated-sidebar-2022-07-18_1-00-43.png?preset=thumbnail";
 import reactSpringTests from "@/assets/pages/react-spring-tests-2021-11-26_01-28-49.png?preset=thumbnail";
 import chSpy from "@/assets/pages/ch-spy.png?preset=thumbnail";
-import testPmDomainLogins from "@/assets/pages/test-pm-domain-logins-2022-03-01_19-09-50.jpg?preset=thumbnail";
-import testPm from "@/assets/pages/test-pm.png?preset=thumbnail";
 import css3d from "@/assets/pages/css-3d-2021-05-24_1-43-59.png?preset=thumbnail";
 import realTimeResizeRcv from "@/assets/pages/real-time-resize-rcv-2022-07-18_1-34-33.png?preset=thumbnail";
 import autoDatGui from "@/assets/pages/auto-dat-gui-2020-11-15_21-24-52.png?preset=thumbnail";
@@ -53,6 +48,14 @@ import svgBkgStripes from "@/assets/pages/svg-bkg-stripes-2022-07-18_18-25-35.pn
 import reactNoPreview from "@/assets/pages/react-no-preview.png?preset=thumbnail";
 import maxzz from "@/assets/pages/maxzz-main.svg?preset=thumbnail";
 import maxzzGithubIo from "@/assets/previews/2022-07-12_18-24-45.png?preset=thumbnail";
+
+//DP
+import testPm from "@/assets/pages/dp/test-pm.png?preset=thumbnail";
+import testPmSecond from "@/assets/pages/dp/test-pm-second-2022-05-21_20-03-21.png?preset=thumbnail";
+import testPmDomainLogins22 from "@/assets/pages/dp/test-pm-domain-logins22-2022-06-29_1-32-01.png?preset=thumbnail";
+import pageQa2 from "@/assets/pages/dp/page-qa2-2022-04-16_16-49-18.png?preset=thumbnail";
+import testPmDomainLogins from "@/assets/pages/dp/test-pm-domain-logins-2022-03-01_19-09-50.jpg?preset=thumbnail";
+import pageExtensionInstallation from "@/assets/pages/dp/page-extension-installation-2022-06-24_17-21-17.png?preset=thumbnail";
 
 export {
     previewDropzon,
