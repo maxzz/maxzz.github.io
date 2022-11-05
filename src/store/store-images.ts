ad77daeb7f66314ef88864e3d80472e755abd284
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-04 18:12:05 -0700

    WIP. store-initials. fix dp preview locations

--- src/store/store-images.ts
+++ src/store/store-images.ts
@@ -1,25 +1,27 @@
-import previewDropzon from "@/assets/pages/dropzone-2022-03-04_15-29-37.png?preset=thumbnail";
-import simpleBorderRadius from "@/assets/pages/simple-border-radius22-2022-07-08_18-46-46.png?preset=thumbnail";
-import red3Curves from "@/assets/pages/red3-curves-2021-08-18_3-57-11.png?preset=thumbnail";
-import borderRadiusGenerator from "@/assets/pages/border-radius-generator-2020-12-18_4-44-41.png?preset=thumbnail";
-import red3Path from "@/assets/pages/red3-path-2021-09-04_19-45-16.png?preset=thumbnail";
-import colorNames22 from "@/assets/pages/color-names22-2022-06-02_3-49-34.png?preset=thumbnail";
-import svgPath from "@/assets/pages/svg-path-2022-02-03_14-18-21-sub-paths.png?preset=thumbnail";
-import levaUiDynamicBoxes from "@/assets/pages/leva-ui-dynamic-boxes-2022-05-03_18-08-19.png?preset=thumbnail";
-import editorsSvgShapes22Template from "@/assets/pages/editors-svg-shapes22-template-2022-04-25_14-17-36.png?preset=thumbnail";
-import ngonGen22 from "@/assets/pages/ngon-gen22-2022-06-11_18-09-01.png?preset=thumbnail";
-import volumeIllusion from "@/assets/pages/volume-illusion-2021-05-04_18-32-05.png?preset=thumbnail";
+import reactNoPreview from "@/assets/pages/react-no-preview.png?preset=thumbnail";
 
-import cursors from "@/assets/pages/cursors-2022-02-20_15-30-06.png?preset=thumbnail";
-import reactSpringTests from "@/assets/pages/react-spring-tests-2021-11-26_01-28-49.png?preset=thumbnail";
-import css3d from "@/assets/pages/css-3d-2021-05-24_1-43-59.png?preset=thumbnail";
-import realTimeResizeRcv from "@/assets/pages/real-time-resize-rcv-2022-07-18_1-34-33.png?preset=thumbnail";
-import reactSm from "@/assets/pages/react-sm-2021-08-05_18-18-46.png?preset=thumbnail";
+//ux, react
+import colorNames22 from "@/assets/pages/color-names22-2022-06-02_3-49-34.png?preset=thumbnail";
+import simpleBorderRadius from "@/assets/pages/simple-border-radius22-2022-07-08_18-46-46.png?preset=thumbnail";
+import borderRadiusGenerator from "@/assets/pages/border-radius-generator-2020-12-18_4-44-41.png?preset=thumbnail";
+import red3Curves from "@/assets/pages/red3-curves-2021-08-18_3-57-11.png?preset=thumbnail";
+import red3Path from "@/assets/pages/red3-path-2021-09-04_19-45-16.png?preset=thumbnail";
 import reactD3Voronoi from "@/assets/pages/react-d3-voronoi-2022-07-18_15-36-06.png?preset=thumbnail";
 import reactD3 from "@/assets/pages/react-d3-2021-07-14_18-55-53.png?preset=thumbnail";
+import svgPath from "@/assets/pages/svg-path-2022-02-03_14-18-21-sub-paths.png?preset=thumbnail";
 import quadraticCurves from "@/assets/pages/quadratic-curves-preview.jpg?preset=thumbnail";
-import giffy from "@/assets/pages/giffy-2021-04-17_21-05-46.png?preset=thumbnail";
+import ngonGen22 from "@/assets/pages/ngon-gen22-2022-06-11_18-09-01.png?preset=thumbnail";
+import editorsSvgShapes22Template from "@/assets/pages/editors-svg-shapes22-template-2022-04-25_14-17-36.png?preset=thumbnail";
+import cursors from "@/assets/pages/cursors-2022-02-20_15-30-06.png?preset=thumbnail";
+import noiseGen from "@/assets/pages/noise-gen-2021-06-14_17-38-45.png?preset=thumbnail";
+import css3d from "@/assets/pages/css-3d-2021-05-24_1-43-59.png?preset=thumbnail";
+import reactSpringTests from "@/assets/pages/react-spring-tests-2021-11-26_01-28-49.png?preset=thumbnail";
+import levaUiDynamicBoxes from "@/assets/pages/leva-ui-dynamic-boxes-2022-05-03_18-08-19.png?preset=thumbnail";
+import volumeIllusion from "@/assets/pages/volume-illusion-2021-05-04_18-32-05.png?preset=thumbnail";
+import realTimeResizeRcv from "@/assets/pages/real-time-resize-rcv-2022-07-18_1-34-33.png?preset=thumbnail";
+import reactSm from "@/assets/pages/react-sm-2021-08-05_18-18-46.png?preset=thumbnail";
 import bloomingGen from "@/assets/pages/blooming-gen-2022-07-18_16-45-15.png?preset=thumbnail";
+import giffy from "@/assets/pages/giffy-2021-04-17_21-05-46.png?preset=thumbnail";
 import reactFocusWithinMenus from "@/assets/pages/react-focus-within-menus-preview-2021-04-01_4-35-18.png?preset=thumbnail";
 import reactGsapMenu from "@/assets/pages/react-gsap-menu-2022-07-18_17-20-08.png?preset=thumbnail";
 import reactSpringInterpolation from "@/assets/pages/react-spring-interpolation-2022-07-18_17-37-52.png?preset=thumbnail";
@@ -27,10 +29,7 @@ import reactDelayUnmount from "@/assets/pages/react-delay-unmount-2021-03-13_2-0
 import reactScrollingCarousel from "@/assets/pages/react-scrolling-carousel-2022-07-18_17-49-30.png?preset=thumbnail";
 import svgIconsFontSw from "@/assets/pages/svg-icons-font-sw-2022-07-18_18-03-13.png?preset=thumbnail";
 import reactAdminAddressBook from "@/assets/pages/react-admin-address-book-2022-07-18_18-16-00.png?preset=thumbnail";
-import reactNoPreview from "@/assets/pages/react-no-preview.png?preset=thumbnail";
 
-//ux, react
-import noiseGen from "@/assets/pages/noise-gen-2021-06-14_17-38-45.png?preset=thumbnail";
 import maxzz from "@/assets/pages/maxzz-main.svg?preset=thumbnail";
 import maxzzGithubIo from "@/assets/previews/2022-07-12_18-24-45.png?preset=thumbnail";
 
@@ -56,6 +55,7 @@ import spawnLoadingTest from "@/assets/pages/npm/spawn-loading-test-2021-04-29_1
 import serverOcr from "@/assets/pages/npm/server-ocr-ocr-text-recognition.jpg?preset=thumbnail";
 
 //DP
+import previewDropzon from "@/assets/pages/dp/dropzone-2022-03-04_15-29-37.png?preset=thumbnail";
 import testPm from "@/assets/pages/dp/test-pm.png?preset=thumbnail";
 import testPmSecond from "@/assets/pages/dp/test-pm-second-2022-05-21_20-03-21.png?preset=thumbnail";
 import testPmDomainLogins22 from "@/assets/pages/dp/test-pm-domain-logins22-2022-06-29_1-32-01.png?preset=thumbnail";
