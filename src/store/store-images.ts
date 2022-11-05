fa68c08cbc2f43e7aeea2e11fd56bf4929216886
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-04 17:52:06 -0700

    WIP. store-initials. update vue preview locations

--- src/store/store-images.ts
+++ src/store/store-images.ts
@@ -11,43 +11,49 @@ import ngonGen22 from "@/assets/pages/ngon-gen22-2022-06-11_18-09-01.png?preset=
 import volumeIllusion from "@/assets/pages/volume-illusion-2021-05-04_18-32-05.png?preset=thumbnail";
 
 import cursors from "@/assets/pages/cursors-2022-02-20_15-30-06.png?preset=thumbnail";
-import noiseGen from "@/assets/pages/noise-gen-2021-06-14_17-38-45.png?preset=thumbnail";
 import npm from "@/assets/pages/npm-node-package-manager.webp?preset=thumbnail";
-import animatedSidebar from "@/assets/pages/animated-sidebar-2022-07-18_1-00-43.png?preset=thumbnail";
 import reactSpringTests from "@/assets/pages/react-spring-tests-2021-11-26_01-28-49.png?preset=thumbnail";
-import chSpy from "@/assets/pages/ch-spy.png?preset=thumbnail";
 import css3d from "@/assets/pages/css-3d-2021-05-24_1-43-59.png?preset=thumbnail";
 import realTimeResizeRcv from "@/assets/pages/real-time-resize-rcv-2022-07-18_1-34-33.png?preset=thumbnail";
-import autoDatGui from "@/assets/pages/auto-dat-gui-2020-11-15_21-24-52.png?preset=thumbnail";
 import reactSm from "@/assets/pages/react-sm-2021-08-05_18-18-46.png?preset=thumbnail";
 import reactD3Voronoi from "@/assets/pages/react-d3-voronoi-2022-07-18_15-36-06.png?preset=thumbnail";
 import reactD3 from "@/assets/pages/react-d3-2021-07-14_18-55-53.png?preset=thumbnail";
-import coursehuntersCourseListBrowserCtx from "@/assets/pages/coursehunters-course-list-browser-ctx-2021-05-07_19-17-13.png?preset=thumbnail";
 import spawnLoadingTest from "@/assets/pages/spawn-loading-test-2021-04-29_17-07-57.png?preset=thumbnail";
 import serverOcr from "@/assets/pages/server-ocr-ocr-text-recognition.jpg?preset=thumbnail";
 import quadraticCurves from "@/assets/pages/quadratic-curves-preview.jpg?preset=thumbnail";
 import giffy from "@/assets/pages/giffy-2021-04-17_21-05-46.png?preset=thumbnail";
-import vue26SiteExamplesTodos from "@/assets/pages/vue26-site-examples-todos-preview.png?preset=thumbnail";
-import backTo90sHtml from "@/assets/pages/back-to-90s-html-2020-12-12_19-48-25.png?preset=thumbnail";
 import bloomingGen from "@/assets/pages/blooming-gen-2022-07-18_16-45-15.png?preset=thumbnail";
 import reactFocusWithinMenus from "@/assets/pages/react-focus-within-menus-preview-2021-04-01_4-35-18.png?preset=thumbnail";
-import ngonGen from "@/assets/pages/ngon-gen-preview-20200512.png?preset=thumbnail";
-import svgGradients from "@/assets/pages/svg-gradients-2020-10-20_21-25-34.png?preset=thumbnail";
-import revuePlayground from "@/assets/pages/revue-playground-2021-03-11_19-21-14.png?preset=thumbnail";
 import reactGsapMenu from "@/assets/pages/react-gsap-menu-2022-07-18_17-20-08.png?preset=thumbnail";
 import reactSpringInterpolation from "@/assets/pages/react-spring-interpolation-2022-07-18_17-37-52.png?preset=thumbnail";
 import reactDelayUnmount from "@/assets/pages/react-delay-unmount-2021-03-13_2-07-43.png?preset=thumbnail";
 import reactScrollingCarousel from "@/assets/pages/react-scrolling-carousel-2022-07-18_17-49-30.png?preset=thumbnail";
-import gluehtml from "@/assets/pages/gluehtml-preview.png?preset=thumbnail";
-import svgIconsVite from "@/assets/pages/svg-icons-vite-2022-07-18_17-58-31.png?preset=thumbnail";
 import svgIconsFontSw from "@/assets/pages/svg-icons-font-sw-2022-07-18_18-03-13.png?preset=thumbnail";
-import simpleSiriwave from "@/assets/pages/simple-siriwave-2020-12-15_17-59-36.png?preset=thumbnail";
-import colorNames from "@/assets/pages/color-names-2020-12-02_4-55-29.png?preset=thumbnail";
 import reactAdminAddressBook from "@/assets/pages/react-admin-address-book-2022-07-18_18-16-00.png?preset=thumbnail";
-import svgBkgStripes from "@/assets/pages/svg-bkg-stripes-2022-07-18_18-25-35.png?preset=thumbnail";
 import reactNoPreview from "@/assets/pages/react-no-preview.png?preset=thumbnail";
+
+//ux, react
+import noiseGen from "@/assets/pages/noise-gen-2021-06-14_17-38-45.png?preset=thumbnail";
 import maxzz from "@/assets/pages/maxzz-main.svg?preset=thumbnail";
 import maxzzGithubIo from "@/assets/previews/2022-07-12_18-24-45.png?preset=thumbnail";
+
+//ux, vue
+import colorNames from "@/assets/pages/vue/color-names-2020-12-02_4-55-29.png?preset=thumbnail";
+import ngonGen from "@/assets/pages/vue/ngon-gen-preview-20200512.png?preset=thumbnail";
+import animatedSidebar from "@/assets/pages/vue/animated-sidebar-2022-07-18_1-00-43.png?preset=thumbnail";
+import chSpy from "@/assets/pages/vue/ch-spy.png?preset=thumbnail";
+import coursehuntersCourseListBrowserCtx from "@/assets/pages/vue/coursehunters-course-list-browser-ctx-2021-05-07_19-17-13.png?preset=thumbnail";
+import autoDatGui from "@/assets/pages/vue/auto-dat-gui-2020-11-15_21-24-52.png?preset=thumbnail";
+import backTo90sHtml from "@/assets/pages/vue/back-to-90s-html-2020-12-12_19-48-25.png?preset=thumbnail";
+import vue26SiteExamplesTodos from "@/assets/pages/vue/vue26-site-examples-todos-preview.png?preset=thumbnail";
+import svgGradients from "@/assets/pages/vue/svg-gradients-2020-10-20_21-25-34.png?preset=thumbnail";
+import revuePlayground from "@/assets/pages/vue/revue-playground-2021-03-11_19-21-14.png?preset=thumbnail";
+import svgIconsVite from "@/assets/pages/vue/svg-icons-vite-2022-07-18_17-58-31.png?preset=thumbnail";
+import simpleSiriwave from "@/assets/pages/vue/simple-siriwave-2020-12-15_17-59-36.png?preset=thumbnail";
+import svgBkgStripes from "@/assets/pages/vue/svg-bkg-stripes-2022-07-18_18-25-35.png?preset=thumbnail";
+
+//npm
+import gluehtml from "@/assets/pages/gluehtml-preview.png?preset=thumbnail";
 
 //DP
 import testPm from "@/assets/pages/dp/test-pm.png?preset=thumbnail";
