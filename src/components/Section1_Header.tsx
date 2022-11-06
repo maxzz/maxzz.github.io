2f25f0cd259b81885c95300e27548b15ebc10862
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-06 15:05:27 -0800

    WIP. header. fix responsiveness on width less then 640px

--- src/components/Section1_Header.tsx
+++ src/components/Section1_Header.tsx
@@ -7,17 +7,17 @@ export function Section1_Header() {
     const styles = useSpring({ from: { x: -200, opacity: 0 }, x: 0, opacity: 1, config: { duration: 4000, easing: easings.easeOutBounce } });
     return (
         <header className="p-4 flex items-center justify-between text-xl bg-black shadow-sm shadow-slate-700">
-            <div className="grid grid-cols-2 items-end space-x-2 text-slate-500 tracking-tighter scale-y-125">
+            <div className="grid grid-rows-[minmax(0,1fr)_1rem] sm:grid-rows-none sm:grid-cols-2 items-end sm:space-x-2 text-slate-500 tracking-tighter scale-y-125">
 
-                <div className="text-4xl ">
+                <div className="text-xl sm:text-4xl">
                     Directory of ...
                 </div>
 
-                <a.div style={styles} className="">
+                <a.div style={styles}>
                     <div className="translate-y-px">
                         <a className="space-x-1" href="https://github.com/maxzz" target="_blank" title="Open Max's GitHub">
-                            <span className="text-sm"> // todo: name to come up with</span>
-                            <IconExperiments className="pb-0.5 w-4 h-4 origin-bottom scale-125 stroke-slate-400 inline-block" />
+                            <span className="text-[.65rem] leading-[.65rem] sm:text-sm"> // todo: name to come up with</span>
+                            <IconExperiments className="pb-0.5 w-4 h-4 origin-bottom scale-75 sm:scale-125 stroke-slate-400 inline-block" />
                         </a>
                     </div>
                 </a.div>
