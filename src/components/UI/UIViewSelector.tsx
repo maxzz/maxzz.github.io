e60ad598295ffea4c66cdad44c5888f72905adf8
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-06 14:13:03 -0800

    WIP. header switch. reduce UIViewSelector movements

--- src/components/UI/UIViewSelector.tsx
+++ src/components/UI/UIViewSelector.tsx
@@ -3,7 +3,7 @@ import { useAtom } from "jotai";
 import { ShowType, uiOptionsAtoms } from "@/store/store";
 import { classNames } from "@/utils/classnames";
 import { IconGrid, IconList } from "./UIIcons";
-
+/*
 function SwitchButtonOld({ thisType, children, ...rest }: { thisType: ShowType; } & HTMLAttributes<HTMLButtonElement>) {
     const [showType, setShowType] = useAtom(uiOptionsAtoms.showTypeAtom);
     return (
@@ -29,7 +29,7 @@ function ViewSelectorOld() {
         </div>
     );
 }
-
+*/
 function SwitchButton({ thisType, children, ...rest }: { thisType: ShowType; } & HTMLAttributes<HTMLButtonElement>) {
     const [showType, setShowType] = useAtom(uiOptionsAtoms.showTypeAtom);
     const isActive = showType === thisType;
@@ -39,11 +39,11 @@ function SwitchButton({ thisType, children, ...rest }: { thisType: ShowType; } &
         <button
             className={classNames(
                 "relative transition-all",
-                " hover:text-primary-300 hover:bg-primary-800 active:scale-[.97]",
+                " hover:text-primary-300 hover:bg-primary-800",
                 isActive ? "text-primary-400 bg-primary-800/70" : "text-primary-600",
                 thisType === ShowType.list && "rounded-tl-[36px] rounded-bl-[36px] rounded-tr-[6px] rounded-br-[6px]",
                 thisType === ShowType.preview && "rounded-tr-[36px] rounded-br-[36px] rounded-tl-[6px] rounded-bl-[6px]",
-                isActive && `after:absolute after:inset-0 after:rounded-[inherit] after:[background:linear-gradient(var(--me),#fff2_-.1%,transparent_75%)]`,
+                isActive && `after:absolute after:inset-0 after:rounded-[inherit] after:[background:linear-gradient(var(--me),#fff1_-.1%,transparent_75%)]`,
             )}
             style={{ boxShadow: '0 0 0 1px #ffffff07, 0 2px 4px #ffffff05, 0 12px 24px #ffffff05', '--me': angle } as React.CSSProperties}
             onClick={() => setShowType(thisType)}
@@ -59,7 +59,7 @@ function SwitchButton({ thisType, children, ...rest }: { thisType: ShowType; } &
 
 export function UIViewSelector() {
     return (
-        <div className="p-0.5 flex items-center space-x-1 bg-slate-800/50 rounded-3xl">
+        <div className="p-0.5 flex items-center space-x-1 bg-slate-800/20 rounded-3xl">
             <SwitchButton thisType={ShowType.list}><><IconList className="p-1 w-6 h-6" /><span>List</span></></SwitchButton>
             <SwitchButton thisType={ShowType.preview}><><IconGrid className="p-1 w-6 h-6" /><span>Grid</span></></SwitchButton>
         </div>
