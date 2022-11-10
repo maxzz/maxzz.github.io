1595ee2996bfbd451c538872819b8f956a66d924
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-09 17:19:33 -0800

    WIP. store-initials. update github projects

--- src/store/store-initials/store-initials-npm.ts
+++ src/store/store-initials/store-initials-npm.ts
@@ -4,16 +4,6 @@ import * as images from "./store-images";
 export const projectsNpm: ProjectType[] = [
     {
         primary: 'npm',
-        id: "work-weeks",
-        description: "Utility to generate working weeks per year.",
-        urlGithub: "https://github.com/maxzz/work-weeks",
-        urlDemo: "https://www.npmjs.com/package/work-weeks",
-        urlPreview: images.npm,
-        localPath: "C:/Y/w/1-node/1-utils/work-weeks-calendar",
-        stack: ['npm'],
-    },
-    {
-        primary: 'npm',
         id: "rardir",
         description: "Utility to pack meta data inside specified folders.",
         urlGithub: "https://github.com/maxzz/rardir",
@@ -34,12 +24,43 @@ export const projectsNpm: ProjectType[] = [
     },
     {
         primary: 'npm',
+        id: "gluehtml",
+        description: "Glue HTML local external files into solid HTML.",
+        urlGithub: "https://www.npmjs.com/package/gluehtml",
+        urlDemo: "https://www.npmjs.com/package/gluehtml",
+        urlPreview: images.gluehtml,
+        localPath: "C:/Y/w/1-node/1-utils/create-single-html",
+        stack: ['npm', 'ts'],
+    },
+    {
+        primary: 'npm',
+        id: "netsh-rule",
+        description: "Utility to generate a batch file to manage inbound and outbound Windows firewall connections for a single .exe file or all .exe files in folder.",
+        urlGithub: "https://github.com/maxzz/netsh-rule",
+        urlDemo: "https://www.npmjs.com/package/netsh-rule",
+        urlPreview: images.npm,
+        localPath: "C:/Y/w/1-node/1-utils/netsh-firewall-rules",
+        stack: ['npm', 'ts'],
+    },
+    {
+        primary: 'npm',
+        id: "topdirs",
+        description: "The topdirs utility replicates the folder structure without copying the files it contains.",
+        urlGithub: "https://github.com/maxzz/topdirs",
+        urlDemo: "https://github.com/maxzz/topdirs", // no demo
+        urlPreview: images.npm,
+        localPath: "C:/Y/w/1-node/1-utils/topdirs",
+        stack: ['npm', 'ts'],
+    },
+    {
+        primary: 'npm',
         id: "spawn-loading-test",
         description: "Node.js test with spawning child processes and a control panel via a web interface.",
         urlGithub: "https://github.com/maxzz/spawn-loading-test",
         urlDemo: "https://github.com/maxzz/spawn-loading-test", // no demo
         urlPreview: images.spawnLoadingTest,
         localPath: "C:/Y/w/1-node/1-utils/spawn-loading-test",
+        stack: ['js'],
     },
     {
         primary: 'npm',
@@ -59,34 +80,16 @@ export const projectsNpm: ProjectType[] = [
         urlDemo: "https://github.com/maxzz/test-graphql", // no demo
         urlPreview: images.npm,
         localPath: "C:/Y/w/2-web/7-my-webpages/test-graphql",
+        stack: ['js'],
     },
     {
         primary: 'npm',
-        id: "gluehtml",
-        description: "Glue HTML local external files into solid HTML.",
-        urlGithub: "https://www.npmjs.com/package/gluehtml",
-        urlDemo: "https://www.npmjs.com/package/gluehtml",
-        urlPreview: images.gluehtml,
-        localPath: "C:/Y/w/1-node/1-utils/create-single-html",
-        stack: ['npm', 'ts'],
-    },
-    {
-        primary: 'npm',
-        id: "netsh-rule",
-        description: "Utility to generate a batch file to manage inbound and outbound Windows firewall connections for a single .exe file or all .exe files in folder.",
-        urlGithub: "https://github.com/maxzz/netsh-rule",
-        urlDemo: "https://www.npmjs.com/package/netsh-rule",
+        id: "work-weeks",
+        description: "Utility to generate working weeks per year.",
+        urlGithub: "https://github.com/maxzz/work-weeks",
+        urlDemo: "https://www.npmjs.com/package/work-weeks",
         urlPreview: images.npm,
-        localPath: "C:/Y/w/1-node/1-utils/netsh-firewall-rules",
-    },
-    {
-        primary: 'npm',
-        id: "topdirs",
-        description: "The topdirs utility replicates the folder structure without copying the files it contains.",
-        urlGithub: "https://github.com/maxzz/topdirs",
-        urlDemo: "https://github.com/maxzz/topdirs", // no demo
-        urlPreview: images.npm,
-        localPath: "C:/Y/w/1-node/1-utils/topdirs",
+        localPath: "C:/Y/w/1-node/1-utils/work-weeks-calendar",
         stack: ['npm', 'ts'],
     },
     {
@@ -97,6 +100,7 @@ export const projectsNpm: ProjectType[] = [
         urlDemo: "https://github.com/maxzz/maxzz-proxy", // no demo
         urlPreview: images.npm,
         localPath: "C:/Y/w/1-node/1-utils/1-web-scraping/maxzz-proxy",
+        stack: ['js'],
     },
     {
         primary: 'npm',
@@ -106,5 +110,6 @@ export const projectsNpm: ProjectType[] = [
         urlDemo: "https://github.com/maxzz/maxzz-python", // no demo
         urlPreview: images.npm,
         localPath: "cannot find the local project anymore",
+        stack: ['python'],
     },
 ];
