1def3d396cb8fa6f11dc3b5c2d15324cb8735452
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-24 16:45:34 -0800

    WIP. icons. add UIIconSymbols.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Section1_Header } from './components/Section1_Header';
 import { Section2_Main } from './components/Section2_Main';
 import { Section3_Footer } from './components/Section3_Footer';
+import { UISymbolDefs } from './components/UI/UIIconSymbols';
 
 function PopperRoot() {
     return (
@@ -13,6 +14,7 @@ function PopperRoot() {
 function App() {
     return (<>
         <PopperRoot />
+        <UISymbolDefs />
         <div className="min-h-full overflow-hidden text-slate-100 bg-slate-900"> {/* debug-screens */}
             <div className="h-screen flex flex-col">
                 <Section1_Header />
