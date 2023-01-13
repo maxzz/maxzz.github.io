f4fbfb04907a544dcab9532b4ed5ce6ef61550c7
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-12 19:42:45 -0800

    WIP. add images for stack group

---
+++ src/store/store-initials/store-images/images-stack.ts
@@ -0,0 +1,8 @@
+//stack
+import wmaxVanillaVite from "@/assets/pages/stack/wmax-vanilla-vite,2023-01-11_13-29-27.png";
+import formsFormik from "@/assets/pages/stack/forms-formik,2023-01-11_13-47-46.png";
+
+export {
+    wmaxVanillaVite,
+    formsFormik,
+};
