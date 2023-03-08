1240d6f18673e1aebf2984ecc80619ae48bd32ef
Max Zakharzhevskiy <maxzz@msn.com>
2023-03-07 18:56:20 -0800

    WIP. update header animation

--- src/components/Section1_Header.tsx
+++ src/components/Section1_Header.tsx
@@ -8,20 +8,38 @@ const textStroke = { WebkitTextStrokeColor: 'var(--tm-primary-500)', WebkitTextS
 function Title() {
     const styles = useSpring({
         from: {
-            'scaley': '10%',
-            'scalex': '50%',
+            scaley: '10%',
+            scalex: '50%',
+            transformOrigin: 'left center',
         },
         to: [
-            { 'scaley': '100%', config: { duration: 200 }, },
-            { 'scaley': '400%', },
-            { 'scaley': '100%', },
-            { 'scalex': '100%', },
+            { scaley: '100%', config: { duration: 200 }, },
+            { scaley: '400%', },
+            { scaley: '100%', },
+            { scalex: '100%', },
         ],
         //config: { duration: 2000, },
     });
+    /*
+    const styles = useSpring({
+        from: {
+            scaley: '10%',
+            scalex: '10%',
+            transformOrigin: 'center',
+        },
+        to: [
+            { scaley: '100%', config: { duration: 100 }, },
+            { scaley: '400%', scalex: '20%', config: { duration: 100 }, },
+            { scaley: '20%', scalex: '100%', config: { duration: 100 } },
+            { scaley: '100%', scalex: '160%', transformOrigin: 'left center', },
+            { scalex: '100%', config: config.wobbly },
+        ],
+        //config: { duration: 2000, },
+    });
+    */
     return (
         <div className="overflow-hidden">
-            <a.div className="text-xl text-primary-700 sm:text-4xl" style={{ ...styles, ...textStroke, transformOrigin: 'left center' }}>
+            <a.div className="text-xl text-primary-700 sm:text-4xl" style={{ ...styles, ...textStroke }}>
                 Directory of ...
             </a.div>
         </div>
