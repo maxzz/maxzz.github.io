116f0f276dce8c4236a9cf9209329e74c35051dd
Max Zakharzhevskiy <maxzz@msn.com>
2025-08-30 13:38:43 -0700

    WIP. update-2025. db-apps. update images path

--- packages/db-apps/src/assets/store-images/1-images-dp.ts
+++ packages/db-apps/src/assets/store-images/1-images-dp.ts
@@ -1,12 +1,12 @@
 //dp
-import previewDropzon from "../../assets/pages/dp/dropzone-2022-03-04_15-29-37.png?preset=thumbnail";
-import testPm from "../../assets/pages/dp/test-pm.png?preset=thumbnail";
-import testPmSecond from "../../assets/pages/dp/test-pm-second-2022-05-21_20-03-21.png?preset=thumbnail";
-import testPmDomainLogins22 from "../../assets/pages/dp/test-pm-domain-logins22-2022-06-29_1-32-01.png?preset=thumbnail";
-import pageQa2 from "../../assets/pages/dp/page-qa2-2022-04-16_16-49-18.png?preset=thumbnail";
-import testPmDomainLogins from "../../assets/pages/dp/test-pm-domain-logins-2022-03-01_19-09-50.jpg?preset=thumbnail";
-import pageExtensionInstallation from "../../assets/pages/dp/page-extension-installation-2022-06-24_17-21-17.png?preset=thumbnail";
-import pmat24Lite from "../../assets/pages/dp/pmat24-lite,2024-03-12 _16-53-42.png?preset=thumbnail";
+import previewDropzon from "../pages/dp/dropzone-2022-03-04_15-29-37.png?preset=thumbnail";
+import testPm from "../pages/dp/test-pm.png?preset=thumbnail";
+import testPmSecond from "../pages/dp/test-pm-second-2022-05-21_20-03-21.png?preset=thumbnail";
+import testPmDomainLogins22 from "../pages/dp/test-pm-domain-logins22-2022-06-29_1-32-01.png?preset=thumbnail";
+import pageQa2 from "../pages/dp/page-qa2-2022-04-16_16-49-18.png?preset=thumbnail";
+import testPmDomainLogins from "../pages/dp/test-pm-domain-logins-2022-03-01_19-09-50.jpg?preset=thumbnail";
+import pageExtensionInstallation from "../pages/dp/page-extension-installation-2022-06-24_17-21-17.png?preset=thumbnail";
+import pmat24Lite from "../pages/dp/pmat24-lite,2024-03-12 _16-53-42.png?preset=thumbnail";
 
 export {
     previewDropzon,
