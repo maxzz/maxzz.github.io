df75fd71a9cd4a1ef96dde2981deb347697b4752
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-05 19:16:08 -0700

    WIP. 2025-update. footer. code review. 4

--- packages/app/src/components/3-footer/1-footer.tsx
+++ packages/app/src/components/3-footer/1-footer.tsx
@@ -5,11 +5,13 @@ import { IconSunnyvale } from "../ui";
 export function Section3_Footer({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     return (
         <div className={classNames("pb-1 h-12 text-xs bg-black text-slate-600 flex items-center justify-center", className)} {...rest}>
-            <a className="hover:underline cursor-pointer" href="https://github.com/maxzz" target="_blank" rel="noopener">Created by Max Zakharzhevskiy.</a>
+            <a className={urlClasses} href="https://github.com/maxzz" target="_blank" rel="noopener">Created by Max Zakharzhevskiy.</a>
 
             <IconSunnyvale className="pt-1 size-8" />
 
-            <a className="hover:underline cursor-pointer" href="https://github.com/maxzz/maxzz.github.io" target="_blank" rel="noopener">Open sourced on GitHub</a>.
+            <a className={urlClasses} href="https://github.com/maxzz/maxzz.github.io" target="_blank" rel="noopener">Open sourced on GitHub</a>.
         </div>
     );
 }
+
+const urlClasses = "hover:underline cursor-pointer";
