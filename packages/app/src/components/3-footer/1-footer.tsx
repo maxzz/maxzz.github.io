4ff0772eb74525ed6a44a77fb01d802b9451a96f
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-05 19:14:05 -0700

    WIP. 2025-update. footer. code review. 2

--- packages/app/src/components/3-footer/1-footer.tsx
+++ packages/app/src/components/3-footer/1-footer.tsx
@@ -4,16 +4,14 @@ import { IconSunnyvale } from "../ui";
 
 export function Section3_Footer({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     return (
-        <div className={classNames("h-12 text-xs bg-black text-slate-600 flex items-center justify-center", className)} {...rest}>
+        <div className={classNames("pb-1 h-12 text-xs bg-black text-slate-600 flex items-center justify-center", className)} {...rest}>
             <p>
-                Created by Max Zakharzhevskiy.
+                <a href="https://github.com/maxzz" target="_blank" rel="noopener">Created by Max Zakharzhevskiy.</a>
             </p>
 
-            <IconSunnyvale className="size-5" />
+            <IconSunnyvale className="pt-1 size-8" />
 
-            <p className="hover:underline cursor-pointer">
-                Open sourced on <a href="https://github.com/maxzz/maxzz.github.io" target="_blank" rel="noopener">GitHub</a>.
-            </p>
+            <a className="hover:underline cursor-pointer" href="https://github.com/maxzz/maxzz.github.io" target="_blank" rel="noopener">Open sourced on GitHub</a>.
         </div>
     );
 }
