67f551b66c91d798ed8b765ffd545539726d28a5
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-15 16:26:20 -0700

    WIP. 2025-update. update definitions layout

--- packages/db-apps/src/store-initials/index.ts
+++ packages/db-apps/src/store-initials/index.ts
@@ -1,9 +1,9 @@
 import { type SectionType } from "./9-types";
 import { projectsDp } from "./1-defs-dp";
-import { projectsNpm } from "./2-defs--npm";
-import { projectsUx } from "./3-defs--react";
-import { projectsSt } from "./4-defs--stack";
-import { projectsVue } from "./5-defs--vue";
+import { projectsNpm } from "./2-defs-npm";
+import { projectsUx } from "./3-defs-react";
+import { projectsSt } from "./4-defs-stack";
+import { projectsVue } from "./5-defs-vue";
 
 export const sections: SectionType[] = [
     {
