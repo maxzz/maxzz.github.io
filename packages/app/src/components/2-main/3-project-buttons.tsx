1bb88353e86a3baf72ce04b90c29ae8604350269
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-24 14:16:38 -0700

    WIP. ProjectButtons. code review. 1

--- packages/app/src/components/2-main/3-project-buttons.tsx
+++ packages/app/src/components/2-main/3-project-buttons.tsx
@@ -7,40 +7,6 @@ import { SymbolGithubLogo, SymbolHardDrive, SymbolTv } from "../ui";
 export function ProjectButtons({ project, inListItem, className, ...rest }: { project: ProjectType; inListItem: boolean; } & HTMLAttributes<HTMLDivElement>) {
     const { urlGithub, urlDemo, localPath, } = project;
 
-    function OpenFromGithub(event: MouseEvent) {
-        if (event.ctrlKey) {
-            event.stopPropagation();
-            event.preventDefault();
-
-            // const link = document.querySelector('.a111') as HTMLAnchorElement;
-            // console.log('link', link);
-            // link?.click();
-
-            window.open(`${urlGithub}/blob/master/package.json`, '_blank');
-        }
-    }
-
-    /*
-    async function OpenFromGithub2(event: MouseEvent) {
-        event.stopPropagation();
-        event.preventDefault();
-
-        try {
-            const body = await fetch(`${urlGithub}/blob/master/package.json`);
-            if (!body.ok) {
-                throw new Error(body.statusText);
-            }
-            const obj = await body.json();
-            console.log('obj', obj);
-        } catch (error) {
-            console.log('tm', error);
-        }
-    }
-    */
-
-    //TODO: open iframe and run clean up script there (but it still github frame w/ limited control for us)
-    //TODO: prebuild github data during this app build
-
     return (
         <div className={classNames("text-xs flex items-center select-none", className)} {...rest}>
             <ButtonShell className={inListItem ? "px-px" : "p-1"} title="Open demo page">
@@ -49,7 +15,7 @@ export function ProjectButtons({ project, inListItem, className, ...rest }: { pr
                 </a>
             </ButtonShell>
 
-            <ButtonShell className={inListItem ? "px-px" : "p-1"} title={"Open source on GitHub\nCtrl+click to open project.json"} onClick={OpenFromGithub}>
+            <ButtonShell className={inListItem ? "px-px" : "p-1"} title={"Open source on GitHub\nCtrl+click to open project.json"} onClick={(event) => OpenFromGithub(event, urlGithub)}>
                 {/* <a href={`${urlGithub}/blob/master/package.json`} className="a111 hidden" target="_blank" aria-label="Open project.json on GitHub"> </a> */}
                 <a href={urlGithub} target="_blank" aria-label="Open source code on GitHub">
                     <SymbolGithubLogo className="w-4 h-4" />
@@ -72,3 +38,35 @@ function ButtonShell({ children, className, ...rest }: HTMLAttributes<HTMLDivEle
         </div>
     );
 }
+
+function OpenFromGithub(event: MouseEvent, urlGithub: string) {
+    if (event.ctrlKey) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        // const link = document.querySelector('.a111') as HTMLAnchorElement;
+        // console.log('link', link);
+        // link?.click();
+
+        window.open(`${urlGithub}/blob/master/package.json`, '_blank');
+    }
+}
+
+async function OpenFromGithub2(event: MouseEvent, urlGithub: string) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    try {
+        const body = await fetch(`${urlGithub}/blob/master/package.json`);
+        if (!body.ok) {
+            throw new Error(body.statusText);
+        }
+        const obj = await body.json();
+        console.log('obj', obj);
+    } catch (error) {
+        console.log('tm', error);
+    }
+}
+
+//TODO: open iframe and run clean up script there (but it still github frame w/ limited control for us)
+//TODO: prebuild github data during this app build
