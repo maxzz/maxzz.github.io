460aa191c77932db5772eb104f96c7983b128fdd
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-24 13:47:47 -0700

    WIP. add new what-if section

---
+++ packages/db-apps/src/store-initials/6-defs-what-if.ts
@@ -0,0 +1,15 @@
+import { type ProjectType } from "./9-types";
+import * as images from "../assets";
+
+export const projectsWhatif: ProjectType[] = [
+    {
+        primary: 'vue',
+        id: "20250920-d3-hierarchical-edge-bundling",
+        description: "CSS color names playground",
+        urlGithub: "https://github.com/maxzz/20250920-d3-hierarchical-edge-bundling",
+        urlDemo: "https://maxzz.github.io/20250920-d3-hierarchical-edge-bundling",
+        urlPreview: images.colorNames,
+        localPath: "C:/y/w/2-web/0-stack/8-technologiy-tests/20250920-d3-hierarchical-edge-bundling",
+        stack: ['vue'],
+    },
+];
--- packages/db-apps/src/store-initials/index.ts
+++ packages/db-apps/src/store-initials/index.ts
@@ -4,6 +4,7 @@ import { projectsNpm } from "./2-defs-npm";
 import { projectsUx } from "./3-defs-react";
 import { projectsSt } from "./4-defs-stack";
 import { projectsVue } from "./5-defs-vue";
+import { projectsWhatif } from "./6-defs-what-if";
 
 export const sections: SectionType[] = [
     {
@@ -25,5 +26,9 @@ export const sections: SectionType[] = [
     {
         name: 'UX, Vue',
         projects: projectsVue,
+    },
+    {
+        name: 'What if...',
+        projects: projectsWhatif,
     },
 ];
