1ceb67fab0f78836abb12af04b54465bff366161
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-25 13:25:08 -0700

    WIP. Update footer and add build at data

--- packages/app/src/components/3-footer/1-footer.tsx
+++ packages/app/src/components/3-footer/1-footer.tsx
@@ -4,14 +4,31 @@ import { IconSunnyvale } from "../ui";
 
 export function Section3_Footer({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     return (
-        <div className={classNames("pb-1 h-12 text-xs bg-black text-slate-600 flex items-center justify-center", className)} {...rest}>
-            <a className={urlClasses} href="https://github.com/maxzz" target="_blank" rel="noopener">Created by Max Zakharzhevskiy.</a>
+        <div className={classNames("pb-2 h-12 text-xs bg-black text-slate-600 flex items-center justify-center", className)} {...rest}>
 
-            <IconSunnyvale className="pt-1 size-8 hover:scale-150 transition-all duration-500" title="Sunnyvale Produce 09.05.2025" />
+            <a className={urlClasses} href="https://github.com/maxzz" target="_blank" rel="noopener">
+                Created by Max Zakharzhevskiy
+            </a>
 
-            <a className={urlClasses} href="https://github.com/maxzz/maxzz.github.io" target="_blank" rel="noopener">Open source code on GitHub.</a>
+            <IconSunnyvale
+                className="pt-1 size-8 hover:scale-150 transition-all duration-300"
+                title={`Sunnyvale Produce\n         ${import.meta.env.VITE_MODIFIED_VERSION}`}
+            />
+
+            <a className={urlClasses} href="https://github.com/maxzz/maxzz.github.io" target="_blank" rel="noopener">
+                Open source code on GitHub
+            </a>
         </div>
     );
 }
 
-const urlClasses = "hover:underline underline-offset-2 hover:text-primary-500 cursor-pointer transition-all";
+const urlClasses = "\
+origin-center \
+underline-offset-2 \
+hover:underline \
+hover:text-primary-500 \
+scale-y-50 \
+hover:scale-y-150 \
+duration-500 \
+transition-colors \
+cursor-pointer";
