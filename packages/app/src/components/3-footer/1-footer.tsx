6bb8646c48460a5826ea6ab7014db83cad4e9e39
Max Zakharzhevskiy <maxzz@msn.com>
2026-06-10 19:58:14 -0700

    WIP. update footer scaling

--- packages/app/src/components/3-footer/1-footer.tsx
+++ packages/app/src/components/3-footer/1-footer.tsx
@@ -4,14 +4,14 @@ import { IconSunnyvale } from "../ui";
 
 export function Section3_Footer({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     return (
-        <div className={classNames("pb-2 h-12 text-xs bg-black text-slate-600 flex items-center justify-center", className)} {...rest}>
+        <div className={classNames("h-12 text-xs bg-black text-slate-600 flex items-center justify-center", className)} {...rest}>
 
             <a className={urlClasses} href="https://github.com/maxzz" target="_blank" rel="noopener">
                 Created by Max Zakharzhevskiy
             </a>
 
             <IconSunnyvale
-                className="pt-1 size-8 hover:scale-150 transition-all duration-300"
+                className="pt-1 size-8 hover:scale-150 transition-all origin-bottom duration-300"
                 title={`${envModifiedDate()}\n   Sunnyvale Produce\n\tVersion ${envBuildVersion()}`}
             />
 
