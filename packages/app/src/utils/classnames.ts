91ddb6b53c5a23a1109bfe16c4b32ef30ddcfec1
Max Zakharzhevskiy <maxzz@msn.com>
2026-08-29 15:02:04 -0700

    WIP. shadcn customization

--- packages/app/src/utils/classnames.ts
+++ packages/app/src/utils/classnames.ts
@@ -1,3 +1,6 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
 export function cx(...classes: Array<string | Record<string, boolean>>): string {
     return classes.map((cls) => (
         typeof cls === 'string'
@@ -8,4 +11,10 @@ export function cx(...classes: Array<string | Record<string, boolean>>): string 
 
 export function classNames(...classes: (string | undefined | false | null)[]): string {
     return classes.filter(Boolean).join(' ');
+}
+
+// shadcn
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
