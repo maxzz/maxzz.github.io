0e0dfb5848184ebd4d2b831ae1b05f4f2380e348
Max Zakharzhevskiy <maxzz@msn.com>
2026-08-29 15:17:59 -0700

    WIP. shadcn customization. 3. add demo

--- packages/app/src/components/0-all/0-app.tsx
+++ packages/app/src/components/0-all/0-app.tsx
@@ -1,18 +1,22 @@
 import { Section1_Header, Section2_Main, Section3_Footer } from "..";
 import { UISymbolDefs } from "../ui/icons/symbols";
+import { Demo } from "../ui/shadcn/demo";
 
 export function App() {
     return (<>
         <PopperRoot />
         <UISymbolDefs />
+
         <div className="h-screen min-h-full overflow-hidden text-slate-100 bg-slate-900 flex flex-col"> {/* debug-screens */}
-            <Section1_Header />
+            {/* <Section1_Header />
 
             <main className="flex-1 my-4 space-y-4 overflow-y-overlay smallscroll">
                 <Section2_Main />
             </main>
 
-            <Section3_Footer />
+            <Section3_Footer /> */}
+
+            <Demo />
         </div>
     </>);
 }
