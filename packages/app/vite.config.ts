d6aa7114ee979e8ad127cf2c99e414f0ff05f82a
Max Zakharzhevskiy <maxzz@msn.com>
2026-09-04 15:43:17 -0700

    WIP. chunks. add modern split. 3

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,10 +1,10 @@
 import path from "node:path";
-import { defineConfig, type Plugin, type PluginOption } from "vite";
+import { type Plugin, type PluginOption, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { visualizer } from "rollup-plugin-visualizer";
-import replace from "@rollup/plugin-replace";
 import imagePresets, { widthPreset } from "vite-plugin-image-presets";
+import replace from "@rollup/plugin-replace";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default (() => defineConfig({
@@ -12,25 +12,10 @@ export default (() => defineConfig({
     plugins: [
         tailwindcss(),
         react(),
-
         thumbnailImagePresets(),
         fixWindowsImagePresetUrls(),
-
-        replace({
-            values: {
-                __BUILD_DATE__: buildAt(),
-                __BUILD_VER__: buildVersion(),
-            },
-            preventAssignment: true,
-        }),
-
-        visualizer({
-            filename: 'visualization.html',
-            template: 'sunburst', // sunburst - d3 style (good as default as well); treemap - table (default); network - graph (slow to open).
-            gzipSize: true,
-            brotliSize: true,
-        }),
-
+        replaceBuildInfo(),
+        bundleVisualizer(),
     ],
     resolve: {
         alias: {
@@ -111,17 +96,6 @@ const NODE_MODULES = '/node_modules/';
 
 //--------------------------------------------------------------------------- 
 
-const buildAt = () => {
-    const d = new Date();
-    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-    return d.toLocaleDateString('en-US', options);
-};
-
-const buildVersion = () => {
-    const d = new Date();
-    return `${d.getFullYear().toString().substring(3)}.${d.getMonth() + 1}${d.getDate()} (${d.getHours()}${d.getMinutes()})`;
-};
-
 function thumbnailImagePresets(): PluginOption {
     return imagePresets({
         thumbnail: widthPreset({
@@ -154,4 +128,35 @@ function fixWindowsImagePresetUrls(): Plugin {
         },
     };
 }
-  
+
+//--------------------------------------------------------------------------- 
+
+const buildAt = () => {
+    const d = new Date();
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    return d.toLocaleDateString('en-US', options);
+};
+
+const buildVersion = () => {
+    const d = new Date();
+    return `${d.getFullYear().toString().substring(3)}.${d.getMonth() + 1}${d.getDate()} (${d.getHours()}${d.getMinutes()})`;
+};
+
+function replaceBuildInfo(): PluginOption {
+    return replace({
+        values: {
+            __BUILD_DATE__: buildAt(),
+            __BUILD_VER__: buildVersion(),
+        },
+        preventAssignment: true,
+    });
+}
+
+function bundleVisualizer(): PluginOption {
+    return visualizer({
+        filename: 'visualization.html',
+        template: 'sunburst', // sunburst - d3 style (good as default as well); treemap - table (default); network - graph (slow to open).
+        gzipSize: true,
+        brotliSize: true,
+    });
+}
